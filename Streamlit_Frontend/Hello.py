b458e11f5aee2313d39bb9566aa8ae10292e6ffc
Yaswanth Kumar Allada <98730871+Yaswanth489@users.noreply.github.com>
2024-02-22 20:34:46 +0530

    First Commit

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Food Diet Recommendation System! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+    You can find more details and the whole project on my [repo](https://github.com/Yaswanth489/Food-Diet-Recommendation-System).
+    """
+)
